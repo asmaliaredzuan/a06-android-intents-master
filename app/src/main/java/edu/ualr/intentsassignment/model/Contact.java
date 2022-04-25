d0ab3059f2b02480f99c700028c8d4e9119f8458
asmaliaredzuan <asmaliaredzuan@gmail.com>
2022-04-25 02:03:15 -0500

    Complete Assignment

---
+++ app/src/main/java/edu/ualr/intentsassignment/model/Contact.java
@@ -0,0 +1,112 @@
+package edu.ualr.intentsassignment.model;
+
+// TODO 05. Modify the Contact class, so you can use it to exchange Contact data between ContactFormActivity and ContactInfoActivity
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Contact implements Parcelable {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String emailAddress;
+    private String address;
+    private String website;
+
+    public Contact(String firstName, String lastName, String phoneNumber, String emailAddress, String address, String website) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.address = address;
+        this.website = website;
+    }
+
+    protected Contact(Parcel in) {
+        firstName = in.readString();
+        lastName = in.readString();
+        phoneNumber = in.readString();
+        emailAddress = in.readString();
+        address = in.readString();
+        website = in.readString();
+    }
+
+    public static final Creator<Contact> CREATOR = new Creator<Contact>() {
+        @Override
+        public Contact createFromParcel(Parcel parcel) {
+            return new Contact(parcel);
+        }
+
+        @Override
+        public Contact[] newArray(int i) {
+            return new Contact[i];
+        }
+    };
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(this.firstName);
+        parcel.writeString(this.lastName);
+        parcel.writeString(this.phoneNumber);
+        parcel.writeString(this.emailAddress);
+        parcel.writeString(this.address);
+        parcel.writeString(this.website);
+    }
+}
